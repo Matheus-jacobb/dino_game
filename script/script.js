c4f3230b7e4c249a3d13445c3885aad547567eff
Matheus Jacob <70240646+Matheus-jacobb@users.noreply.github.com>
2021-02-13 20:45:41 -0300

    [MODIFY] Naruto theme

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 /* let: can be change || const: cant be change */
 
-const dino = document.querySelector('.dino'); // Save object dino at const dino
+const player = document.querySelector('.player'); // Save object player at const player
 const background = document.querySelector('.background'); 
 let isJumping = false;
 let position = 0;
 let reload = false;
-let cactusCont = 0;
+let obstacleCont = 0;
 
 function handleKeyUp(event){
     if (event.keyCode == 32){ // KeyCode of space (www.keycode.info)
@@ -31,46 +31,46 @@ function jump (){
             }
             else{
                 position -= 20;
-                dino.style.bottom = position + 'px';
+                player.style.bottom = position + 'px';
             }
              },20)
         }   
         else
             position += 20;
 
-        dino.style.bottom = position + 'px';
+        player.style.bottom = position + 'px';
     }, 20) // Atualiza a cada 20ms
 }
 
-    function createCactus() {
-        const cactus = document.createElement('div');
-        let cactusPosition = 2500;
+    function createObstacle() {
+        const obstacle = document.createElement('div');
+        let obstaclePosition = 2500;
         let randomTime = Math.random() * 6000;
-        cactusCont ++;
+        obstacleCont ++;
 
 
-        cactus.classList.add('cactus');
-        cactus.style.left = 2500 + 'px';
-        background.appendChild(cactus);
+        obstacle.classList.add('obstacle');
+        obstacle.style.left = 2500 + 'px';
+        background.appendChild(obstacle);
 
         let leftInterval = setInterval(() => {
-            if(cactusPosition < -60){
+            if(obstaclePosition < -60){
                 clearInterval(leftInterval);
-                background.removeChild(cactus);
+                background.removeChild(obstacle);
             }
-            else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            else if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
                 clearInterval(leftInterval);
                 document.body.innerHTML = '<div class = "endgame"><h1 class = "game-over">GAME OVER</h1> <h2 class= "continue"> PRESS SPACE TO CONTINUE</h2></div>'; //add HTML page (GAMEOVER)
                 reload = true;                
             }
             else{
-                cactusPosition -= (10 + cactusCont); // cactus speed [variable]
-                cactus.style.left = cactusPosition + 'px';
+                obstaclePosition -= (10 + obstacleCont); // obstacle speed [variable]
+                obstacle.style.left = obstaclePosition + 'px';
             }
         },20)
 
-        setTimeout(createCactus, randomTime);
+        setTimeout(createObstacle, randomTime);
 
     }
-createCactus();
+createObstacle();
 document.addEventListener('keydown', handleKeyUp);
